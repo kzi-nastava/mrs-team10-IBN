392c675bb55f2c2cbda0cbf0221cefc26fa3ff05
Ivana Ignjatic <ignjaticivana70@gmail.com>
2025-12-12 14:38:03 +0100

    [ADD] nav-bar and side-bar created

--- IKS/uber/src/app/app.ts
+++ IKS/uber/src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavBarComponent} from './layout/nav-bar/nav-bar.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('uber');
 }
+
+
