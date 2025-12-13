2e3a27cfdaf9cb0a772be4802007fc9f885d1215
paunovicbojana <paunovicboka@gmail.com>
2025-12-13 23:56:41 +0100

    [ADD]: profile update and request review

--- IKS/uber/src/app/app.routes.ts
+++ IKS/uber/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './forms/login/login.component';
 import { RegisterComponent } from './forms/register/register.component';
 import { HomeComponent } from './layout/home/home.component';
 import { AccountComponent } from './layout/account/account.component';
+import { RequestsComponent } from './layout/requests/requests.component';
 
 export const routes: Routes = [
   {
@@ -16,6 +17,10 @@ export const routes: Routes = [
   {
     path: 'account',
     component: AccountComponent,
+  },
+  {
+    path: 'requests',
+    component: RequestsComponent,
   },
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
