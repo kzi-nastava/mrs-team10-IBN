75b02370b3f98ab102f6a9c8eee7f504aca8c0da
niksa <cvorovicniksa@gmail.com>
2025-12-18 23:46:32 +0100

    [ADD] Register Activity UI implemented

--- MOBILNE/app/src/main/java/com/example/ubercorp/activities/RegisterActivity.java
+++ MOBILNE/app/src/main/java/com/example/ubercorp/activities/RegisterActivity.java
@@ -1,16 +1,43 @@
 package com.example.ubercorp.activities;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.ubercorp.R;
 
+import java.net.URI;
+
 public class RegisterActivity extends AppCompatActivity {
+    final int MEDIA_IMAGES_PERMISSION = 100;
+    final int SELECT_IMAGE = 200;
+    ImageView profilePicture;
+
+    private void selectImage(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Profile Picture"), SELECT_IMAGE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +49,32 @@ public class RegisterActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        profilePicture = findViewById(R.id.profilePicture);
+        Button selectImageButton = findViewById(R.id.selectImageButton);
+        selectImageButton.setOnClickListener((v) -> {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_MEDIA_IMAGES)
+                    != PackageManager.PERMISSION_GRANTED){
+                requestPermissions(new String[]{Manifest.permission.READ_MEDIA_IMAGES}, MEDIA_IMAGES_PERMISSION);
+            } else {
+                selectImage();
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && requestCode == SELECT_IMAGE){
+            Uri uri = data.getData();
+            profilePicture.setImageURI(uri);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults, int deviceId) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults, deviceId);
+        if (requestCode == MEDIA_IMAGES_PERMISSION && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            selectImage();
+        }
     }
 }
