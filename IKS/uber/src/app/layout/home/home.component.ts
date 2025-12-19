36df0891bdfacc2dcacf11550e4b9e88a277c377
Ivana Ignjatic <ignjaticivana70@gmail.com>
2025-12-19 19:52:43 +0100

    [ADD] integartion of map to home page, with pin and car icons

--- IKS/uber/src/app/layout/home/home.component.ts
+++ IKS/uber/src/app/layout/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MapComponent } from '../../map/map.component';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterModule],
+  imports: [RouterModule, MapComponent, NavBarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
