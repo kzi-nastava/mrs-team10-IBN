f0837d11eca2027d79454a838fd22b6a520a67b5
Ivana Ignjatic <ignjaticivana70@gmail.com>
2025-12-21 16:34:59 +0100

    [ADD] spring initializer

---
+++ ISS/UberComp/src/test/java/com/example/UberComp/UberCompApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UberComp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberCompApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
