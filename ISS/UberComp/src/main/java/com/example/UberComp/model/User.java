dbb79f20489ee75d0e477053bd00ca04cd3c3d34
niksa <cvorovicniksa@gmail.com>
2026-01-04 23:16:57 +0100

    [FIX] Stack overflow caused by Lombok

--- ISS/UberComp/src/main/java/com/example/UberComp/model/User.java
+++ ISS/UberComp/src/main/java/com/example/UberComp/model/User.java
@@ -1,14 +1,14 @@
 package com.example.UberComp.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
-@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity(name = "app_user")
 public class User {
     @Id
