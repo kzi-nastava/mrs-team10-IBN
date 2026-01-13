4929c76bd8be69e96ecc71b8e91b48d36efa78ed
Ivana Ignjatic <ignjaticivana70@gmail.com>
2026-01-13 20:59:10 +0100

    [ADD] backend for vehicle pricing

--- IKS/uber/src/app/forms/vehicle-price/vehicle-price.component.ts
+++ IKS/uber/src/app/forms/vehicle-price/vehicle-price.component.ts
@@ -1,12 +1,12 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
-export interface VehiclePriceData {
-  standard: number;
-  luxury: number;
-  van: number;
-}
+import { VehiclePriceData } from '../../model/vehicle-price.model';
+import { signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { MatDialog } from '@angular/material/dialog';
+import { SimpleMessageDialogComponent } from '../../layout/simple-message-dialog/simple-message-dialog.component';
 
 @Component({
   selector: 'app-vehicle-price',
@@ -17,21 +17,39 @@ export interface VehiclePriceData {
 })
 export class VehiclePriceComponent {
 
-  prices: VehiclePriceData = {
-    standard: 320,
-    luxury: 440,
-    van: 400,
-  };
+  public prices = signal<VehiclePriceData[]>([]);
+
+  constructor(private http: HttpClient, private dialog: MatDialog){
+    this.loadVehiclePrices();
+
+  }
 
   @Output() closeModal = new EventEmitter<void>();
   @Output() savePrices = new EventEmitter<VehiclePriceData>();
+
+  loadVehiclePrices(){
+    this.http.get<VehiclePriceData[]>(`${environment.apiHost}/prices`).
+    subscribe(prices => this.prices.set(prices));
+    console.log(this.prices)
+  }
 
   close() {
     this.closeModal.emit();
   }
 
   save() {
-    this.savePrices.emit(this.prices);
+    console.log(this.prices()[0])
+    this.http.put(`${environment.apiHost}/prices`, this.prices()).subscribe({
+        next: () => {
+          this.dialog.open(SimpleMessageDialogComponent, {
+          width: '300px',
+          data: { message: "Prices are saved." }
+        });
+        },
+        error: (err) => {
+          console.error('Error posting review', err);
+        }
+      });
   }
 
   stopPropagation(event: Event) {
