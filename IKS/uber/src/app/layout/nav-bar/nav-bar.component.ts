3d8bd65937bb2fa8e48a1d62ccedda5f47ce996e
Ivana Ignjatic <ignjaticivana70@gmail.com>
2026-01-26 14:11:14 +0100

    [ADD] incoming ride to navbar

--- IKS/uber/src/app/layout/nav-bar/nav-bar.component.ts
+++ IKS/uber/src/app/layout/nav-bar/nav-bar.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../service/auth.service';
@@ -21,15 +21,17 @@ import { UpdateLocationComponent } from '../update-location/update-location.comp
 export class NavBarComponent {
   isDriver = false;
   loggedIn: boolean;
+  role: string | null;
   router: Router;
-  constructor(authService: AuthService, router: Router){
-    this.loggedIn = authService.isLoggedIn()
-    this.router = router
+  constructor(authService: AuthService, router: Router) {
+    this.loggedIn = authService.isLoggedIn();
+    this.role = authService.role();
+    this.router = router;
   }
 
   logout() {
     localStorage.clear();
     this.loggedIn = false;
-    this.router.navigate(["/home"]);
+    this.router.navigate(['/home']);
   }
 }
